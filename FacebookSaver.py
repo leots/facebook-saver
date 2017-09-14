001aeaaff14c911882028d36cbe551bc32f71644
Leonidas Tsekouras <leotsekouras@gmail.com>
2017-09-14 15:46:56 +0300

    Get posts from the Facebook API for the specified pages

--- FacebookSaver.py
+++ FacebookSaver.py
@@ -5,11 +5,13 @@
 import json
 
 import mysql.connector
+import requests
 
 
 def get_local_json_contents(json_filename):
     """
     Return the contents of a (local) JSON file
+
     :param json_filename: the filename (as a string) of the local JSON file
     :returns: the data of the JSON file
     """
@@ -28,13 +30,17 @@ def get_local_json_contents(json_filename):
     return data
 
 
-def main():
-    # Read parameters from JSON properties file
-    properties = get_local_json_contents("properties.json")
+def save_posts_to_db(properties, page, posts):
+    """
+    Save the given posts into a MySQL database
 
-    # Read test JSON Facebook response
-    fb_json = get_local_json_contents("test.json")
-    fb_json = fb_json["data"]
+    :param page: ID of the FB page that the posts were retrieved from
+    :param properties: Database host, username and password
+    :param posts: The posts, as retrieved from the Facebook API
+    :return: -
+    """
+
+    no_more_pages = False
 
     # Connect to database
     cnx = mysql.connector.connect(user=properties["mysql_user"],
@@ -44,15 +50,12 @@ def main():
     cursor = cnx.cursor()
 
     # Add posts to the database
-    for post in fb_json:
+    for post in posts:
         if "message" in post:
-            fb_page = "topotami"
+            fb_page = page
             post_id = post["id"]
             timestamp = post["created_time"]
             message = post["message"]
-
-            print(fb_page + " (" + timestamp + ") [" + post_id + "]: "
-                  + message[0:40] + "[...]\nAdding to database...")
 
             # Insert post into database
             add_post = ("INSERT INTO posts "
@@ -63,12 +66,14 @@ def main():
 
             try:
                 cursor.execute(add_post, post_data)
-            except mysql.connector.errors.IntegrityError:
-                print("Duplicate entry! Skipping...")
-        else:
-            print("Story ignored...")
+            except mysql.connector.errors.IntegrityError as err:
+                print("Duplicate entry! Stopping for page. (" + err.msg + ")")
 
-        print()
+                no_more_pages = True
+                break
+        else:
+            # todo: do something with stories (?)
+            print("Story ignored...")
 
     # Commit changes to the database
     cnx.commit()
@@ -76,6 +81,69 @@ def main():
     # Close the cursor & database connection
     cursor.close()
     cnx.close()
+
+    # Return whether there are more posts or not
+    return no_more_pages
+
+
+def get_facebook_posts(token, page_id, offset, limit):
+    """
+    Get a number of Facebook posts of a page using the Facebook Graph API,
+    with the given offset.
+
+    :param token: Facebook API token
+    :param page_id: The ID of the page to get posts of
+    :param offset: Where to start getting posts from
+    :param limit: How many posts to fetch. Upper limit from Facebook is 100.
+    :return: The retrieved posts, in JSON format
+    """
+
+    url = "https://graph.facebook.com/v2.10/" + page_id + "/feed"
+
+    request_params = {
+        "access_token": token,
+        "offset": offset,
+        "limit": limit
+    }
+
+    r = requests.get(url, params=request_params)
+
+    # Return the response, parsed as JSON
+    return json.loads(r.text)
+
+
+def main():
+    # Read parameters from JSON properties file
+    properties = get_local_json_contents("properties.json")
+    fb_api_token = properties["facebook_token"]
+    pages_to_watch = properties["facebook_pages"]
+
+    page_size = 100
+
+    # Get Facebook posts for each of the pages
+    for page in pages_to_watch:
+        print("Getting posts for page: " + page)
+
+        offset = 0
+        while True:
+            # Get Facebook posts
+            print("Requesting posts from " + str(offset) + " to "
+                  + str(offset + page_size) + "...")
+
+            fb_json = get_facebook_posts(fb_api_token, page, offset, page_size)
+            fb_json = fb_json["data"]
+
+            print("\t" + str(len(fb_json)) + " posts on this page")
+
+            # Save these posts to the database
+            done = save_posts_to_db(properties, page, fb_json)
+
+            # If a duplicate post was found or there are no more posts, move on
+            if done or len(fb_json) == 0:
+                print("Done with " + page + ", moving to next site\n\n")
+                break
+            else:
+                offset += page_size
 
 
 if __name__ == "__main__":
