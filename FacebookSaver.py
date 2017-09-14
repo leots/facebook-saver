3b8503251bc4db5f964bb8ed165b0273b873c59a
Leonidas Tsekouras <leotsekouras@gmail.com>
2017-09-14 12:44:05 +0300

    Add reading MySQL properties from JSON file

--- FacebookSaver.py
+++ FacebookSaver.py
@@ -3,4 +3,37 @@
     https://stackoverflow.com/a/25724855
 """
 
+import json
 
+
+def get_local_json_contents(json_filename):
+    """
+    Return the contents of a (local) JSON file
+    :param json_filename: the filename (as a string) of the local JSON file
+    :returns: the data of the JSON file
+    """
+
+    try:
+        with open(json_filename) as json_file:
+            try:
+                data = json.load(json_file)
+            except ValueError:
+                print("Contents of '" + json_filename + "' are not valid JSON")
+                raise
+    except IOError:
+        print("An error occurred while reading the '" + json_filename + "'")
+        raise
+
+    return data
+
+
+def main():
+    print("Reading properties.json...\n")
+
+    properties = get_local_json_contents("properties.json")
+
+    print(properties)
+
+
+if __name__ == "__main__":
+    main()
